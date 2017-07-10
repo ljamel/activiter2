2532e0ebb1c0f25b5ff01fba424126be102649bc
pubs1896 <djamallamri@yahoo.fr>
2017-07-10 18:46:11 +0200

    Ajout de commentaires pour se reperer

--- js/detecte.js
+++ js/detecte.js
@@ -1,10 +1,12 @@
 Format = {
+    // Affiche la largeur de l'ecran dans l'objet arg
     larg: (document.body.clientWidth),
 
     Largeur: function () {
         console.log("La largeur de mon ecrant et de " + Format.larg);
         document.getElementById('larg').innerHTML = "La largeur de mon ecrant et de " + Format.larg + "<br /> " + "Le modèl de votre navigateur est " + navigator.appVersion + " ";
 
+        // Ajout de condition pour afficher un message pour chaque format d'écran
         if (Format.larg < 700) {
             document.getElementById('larg2').innerHTML = "Vous avez un petit ecrant ";
         }
@@ -19,8 +21,7 @@ Format = {
 
     },
 
-
-
 };
 
+// Appel la fonction qui se trouver dans un l'objet Format avec la méthode Largeur = Format.Largeur();
 Format.Largeur();
