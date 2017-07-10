9fbcafb1f74e06539434a2c2ea85f8509e28a21a
pubs1896 <djamallamri@yahoo.fr>
2017-07-10 15:12:11 +0200

    Affichage de message selon la taille de lecrant

--- js/detecte.js
+++ js/detecte.js
@@ -4,6 +4,15 @@ Format = {
     Largeur: function () {
         console.log("La largeur de mon ecrant et de " + Format.larg);
         document.getElementById('larg').innerHTML = "La largeur de mon ecrant et de " + Format.larg;
+        if (Format.larg < 700) {
+            document.getElementById('larg2').innerHTML = "Vous avez un petit ecrant ";
+        }
+        if (Format.larg > 700 && Format.larg < 1200) {
+            document.getElementById('larg2').innerHTML = "Vous avez un ecran moyen ";
+        }
+        if (Format.larg > 1200) {
+            document.getElementById('larg2').innerHTML = "Vous avez un ecran grand écrant ";
+        }
     },
 
 };
