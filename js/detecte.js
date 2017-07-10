16336ad1411f4d70f49c343b781ce139e962600c
pubs1896 <djamallamri@yahoo.fr>
2017-07-10 16:08:25 +0200

    Affichage du model de navigateur

--- js/detecte.js
+++ js/detecte.js
@@ -3,17 +3,23 @@ Format = {
 
     Largeur: function () {
         console.log("La largeur de mon ecrant et de " + Format.larg);
-        document.getElementById('larg').innerHTML = "La largeur de mon ecrant et de " + Format.larg;
+        document.getElementById('larg').innerHTML = "La largeur de mon ecrant et de " + Format.larg + "<br /> " + "Le modèl de votre navigateur est " + navigator.appVersion + " ";
+
         if (Format.larg < 700) {
             document.getElementById('larg2').innerHTML = "Vous avez un petit ecrant ";
         }
+
         if (Format.larg > 700 && Format.larg < 1200) {
             document.getElementById('larg2').innerHTML = "Vous avez un ecran moyen ";
         }
+
         if (Format.larg > 1200) {
             document.getElementById('larg2').innerHTML = "Vous avez un ecran grand écrant ";
         }
+
     },
+
+
 
 };
 
